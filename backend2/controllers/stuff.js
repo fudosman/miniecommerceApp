0e5a9285c6b01b270f7914c03457060e9d4caf74
afudoh kachi <fudosman@gmail.com>
2021-07-23 14:33:11 +0100

    friday modification on the delete route

--- backend2/controllers/stuff.js
+++ backend2/controllers/stuff.js
@@ -80,16 +80,23 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
+  Thing.findOne({_id: req.params.id}).then(
+    (thing) => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink('images/' + filename, () => {
+        Thing.deleteOne({_id: req.params.id}).then(
+          () => {
+            res.status(200).json({
+              message: 'Deleted!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
       });
     }
   );
