3bbe7ecb918b364d9a36aaeadb6e2a8dd9cd60f9
afudoh kachi <fudosman@gmail.com>
2021-07-23 14:23:19 +0100

    friday commit updating the post and put routes

--- backend2/app.js
+++ backend2/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
-
+const path = require('path');
 const app = express();
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb://localhost:27017/myapp')
+mongoose.connect('mongodb://localhost:27017/myapp',{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Successfully connected to MongoDB Atlas!');
   })
@@ -13,109 +14,24 @@ mongoose.connect('mongodb://localhost:27017/myapp')
     console.error(error);
   });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-// for parsing application/json
-app.use(express.json()) ;
-// for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })) ;
-
-app.post('/api/stuff', (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
+  
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
   });
-  thing.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id
-  }).then(
-    (thing) => {
-      res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+  
+  // for parsing application/json
+  app.use(express.json()) ;
+  // for parsing application/x-www-form-urlencoded
+  app.use(express.urlencoded({ extended: true })) ;
+  // for adding images to our image folder
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  
+  // the routes are registered on the /routes folder 
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
+
